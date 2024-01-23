de04cdd423189bc7ea85358baf2c1e22c9ac41d0
Yahya Gilany <yahya.gilany@live.com>
2024-01-23 10:58:24 -0500

    Refactor password-reveal.spec.ts

--- tests/password-reveal.spec.ts
+++ tests/password-reveal.spec.ts
@@ -1,11 +1,10 @@
-import exp from "constants";
 import path from "path";
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test.describe('Password Reveal', () => {
+test.describe("Password Reveal", () => {
   const credentials = {
     username: `user`,
-    password: `secret-password`
+    password: `secret-password`,
   };
 
   test.beforeEach(async ({ page }) => {
@@ -13,31 +12,31 @@ test.describe('Password Reveal', () => {
     await page.goto(`file://${filePath}`);
   });
 
-  test('displays a form', async ({ page }) => {
+  test("displays a form", async ({ page }) => {
     await expect(page.getByLabel(`username`)).toBeVisible();
     await expect(page.getByLabel(`password`)).toBeVisible();
   });
 
-  test('can fill out a form', async ({ page }) => {
+  test("can fill out a form", async ({ page }) => {
     await fillOutForm(page);
 
-    await expect(page.getByLabel(`username`)).toHaveValue(credentials.username)
-    await expect(page.getByLabel(`password`)).toHaveValue(credentials.password)
+    await expect(page.getByLabel(`username`)).toHaveValue(credentials.username);
+    await expect(page.getByLabel(`password`)).toHaveValue(credentials.password);
   });
 
-  test('can toggle password visibility', async ({ page }) => {
+  test("can toggle password visibility", async ({ page }) => {
     await fillOutForm(page);
 
-    await expect(page.getByLabel(`username`)).toHaveValue(credentials.username)
-    await expect(page.getByLabel(`password`)).toHaveValue(credentials.password)
+    await expect(page.getByLabel(`username`)).toHaveValue(credentials.username);
+    await expect(page.getByLabel(`password`)).toHaveValue(credentials.password);
 
-    await expect(page.getByLabel(`password`)).toHaveAttribute(`type`, `password`)
-    
+    await expect(page.getByLabel(`password`)).toHaveAttribute(`type`, `password`);
+
     await page.getByTestId(`toggle-password-visibility`).click();
-    await expect(page.getByLabel(`password`)).toHaveAttribute(`type`, `text`)
-    
+    await expect(page.getByLabel(`password`)).toHaveAttribute(`type`, `text`);
+
     await page.getByTestId(`toggle-password-visibility`).click();
-    await expect(page.getByLabel(`password`)).toHaveAttribute(`type`, `password`)
+    await expect(page.getByLabel(`password`)).toHaveAttribute(`type`, `password`);
   });
 
   async function fillOutForm(page) {
